ad10cd022dfefa9e907d4b27f519ecef4ce7fed1
aloycejpm <firstajep@gmail.com>
2018-06-06 10:28:04 +0300

    change styles

--- src/main/java/com/niafikra/olis/MainView.java
+++ src/main/java/com/niafikra/olis/MainView.java
@@ -4,12 +4,14 @@ import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.page.Viewport;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
 
-@HtmlImport("styles/shared-styles.html")
+@HtmlImport("frontend://styles/shared-styles.html")
+@Viewport("width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes")
 @Route("")
 @Theme(Lumo.class)
 
